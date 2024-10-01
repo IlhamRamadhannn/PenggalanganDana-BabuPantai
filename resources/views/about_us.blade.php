aecfadbb07c264d2b2cdfb2ddc3ef06c21f77f5d
c4lv1 <calvinwinatha88@gmail.com>
2024-10-01 23:28:03 +0700

    homepage layout section

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -7,9 +7,15 @@
     <title>about us</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <style >
-        div.row
-    {
-            border: 1px solid rgb(188, 8, 8);
+        div{
+            border: 1px solid rgb(0, 0, 0);
+            
+        }
+        p{
+            
+        }
+        *{
+            
         }
     </style>
 </head>
@@ -17,11 +23,63 @@
 
     <div class="container">
         <div class= "row">
-            <div class="col-8">
-                <h1>hellyea</h1>
+            <div id="logo" class="col-8 ">
+                <p>logo</p>
+            </div>
+            
+            <div id="about us" class="col-1 ">
+                <p>about</p>
+            </div>
+
+            <div id="donate" class="col-1 ">
+                <p>donate</p>
+            </div>
+           
+            <div id="my donation" class="col-1 " sty>
+                <p>my donation</p>
+            </div>
+            <div id="profile" class="col-1 ">
+                <p>profile</p>
+            </div>
+            
+        </div>
+         <br><br><br><br>
+        <div class="row"  style="height: 50px ; text-align: center ">
+            
+            <div class="col-12" sty>
+                <p> judul</p>
+            </div>
+            
+        </div>
+       
+        <div class="row"  style="height: 700px ; text-align: center">
+           <div class="col-12">
+            <p>CONTENT</p>
+            <img src="squiward_ngemis.png" alt="" style="height: 50 vh">
+           </div>
+            
+        </div>
+        <div class="row">
+            <div class="col-4">
+
             </div>
             <div class="col-4">
-                <h1>hellno</h1>
+                <img src="image.png" alt="" style="height: 50vh">
+            </div>
+            <div class="col-4">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12" style="text-align: center; height: 70px;">
+                
+                <button> DONATE !</button>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12"style="text-align: center; height: 100px">
+                footer
             </div>
         </div>
     </div>
