f5025524b310016c40727fb83cd64b83bd915595
c4lv1 <calvinwinatha88@gmail.com>
2024-10-01 22:07:15 +0700

    with bootstrap updated commit

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about us</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <style >
+        div.row
+    {
+            border: 1px solid rgb(188, 8, 8);
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class= "row">
+            <div class="col-8">
+                <h1>hellyea</h1>
+            </div>
+            <div class="col-4">
+                <h1>hellno</h1>
+            </div>
+        </div>
+    </div>
+   
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/', function () {
     echo "123";
     return view('welcome');
 });
+Route::get('/about', function(){
+    return view('about_us');
+});
