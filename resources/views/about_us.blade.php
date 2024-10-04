48eebb3b015320ffcfea8652838a8505aa9bc587
IlhamRamadhannn <ilhamsr05@gmail.com>
2024-10-04 23:02:27 +0700

    Updated Background / and edited css

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <style >
         div{
-            border: 1px solid rgb(0, 0, 0);
+            /* border: 1px solid rgb(0, 0, 0); */
             
         }
         p{
@@ -17,44 +17,64 @@
         *{
             
         }
+
+        body {
+        background-image: url('/images/backgr_00000.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    .navbar-nav .nav-link {
+            color: white; /* Set link text color to white */
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: #007bff; /* Change link color on hover to blue */
+        }
     </style>
 </head>
 <body>
 
-    <div class="container">
+    <div class="container-fluid"  style=" position: fixed; top: 0;">
         <div class= "row">
-            <div id="logo" class="col-8 ">
-                <p>logo</p>
+            <div id="logo" class="col-8 p-4">
+                <img src="{{url('/images/logo3_00000.png')}}" alt="" style="height: 90px; width: 265px;">
             </div>
-            
-            <div id="about us" class="col-1 ">
-                <p>about</p>
-            </div>
-
-            <div id="donate" class="col-1 ">
-                <p>donate</p>
-            </div>
-           
-            <div id="my donation" class="col-1 " sty>
-                <p>my donation</p>
-            </div>
-            <div id="profile" class="col-1 ">
-                <p>profile</p>
-            </div>
+            <div class="col-4 d-flex justify-content-center align-items-center">
+                <nav class="navbar navbar-expand-xxl">
+                    <ul class="navbar-nav" style="color:white;">
+                        <li class="nav-item p-3">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item p-3">
+                            <a class="nav-link" href="#">Donate</a>
+                        </li>
+                        <li class="nav-item p-3">
+                            <a class="nav-link" href="#">My Donate</a>
+                        </li>
+                        <li class="nav-item p-3">
+                            <a class="nav-link" href="#">Profile</a>
+                        </li>
+            </ul>
+            </nav>
+        </div>
+    </div>
             
         </div>
          <br><br><br><br>
-        <div class="row"  style="height: 50px ; text-align: center ">
-            
+         <br><br><br><br>
+        <div class="row"  style="height: 50px ; text-align: left">
             <div class="col-12" sty>
-                <p> judul</p>
-            </div>
+                <h5 class= "card-title" style="font-size: 65px; color: #4A90E2; font-weight: bold; padding-left: 55px; "> LIFE BELOW THE WATER</h5>
+        </div>
             
         </div>
        
-        <div class="row"  style="height: 700px ; text-align: center">
+        <div class="row"  style="height: 700px ; text-align: left; color: white; font-weight: bold;  padding-left: 55px;  padding-right: 55px; padding-top: 50px; text-shadow: 4px 4px 12px rgba(0, 0, 0, 0.3);">
            <div class="col-12">
-            <p>CONTENT</p>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Euismod auctor tincidunt sed aliquam placerat commodo. Porttitor penatibus pellentesque ridiculus himenaeos volutpat dictum libero sodales. Ac accumsan maximus ligula mus diam ullamcorper faucibus tincidunt. Montes metus phasellus ridiculus vehicula dui inceptos. Ipsum vel euismod facilisis facilisis hendrerit vehicula nascetur aliquam. Nec conubia penatibus ex faucibus rhoncus posuere. Torquent euismod nascetur, montes augue cras class consectetur odio. Ipsum viverra neque ultricies, massa nisi senectus.
+Nam ut ac diam euismod faucibus ultricies maecenas penatibus suspendisse. Tristique convallis inceptos ex sociosqu natoque praesent; ante vehicula lectus. Natoque suscipit laoreet metus sodales consequat justo metus ultrices. Dui tortor mollis tempor habitant inceptos mus netus rhoncus. Quis sed fermentum tortor integer curabitur libero vehicula molestie. Faucibus semper ligula posuere per varius cubilia hendrerit. Dolor ut a habitasse duis dictum montes taciti. Turpis sit sed nibh enim amet. Habitant adipiscing posuere euismod consequat auctor aptent turpis erat. Eros per sollicitudin posuere nulla ac.
+</p>
             <img src="squiward_ngemis.png" alt="" style="height: 50 vh">
            </div>
             
