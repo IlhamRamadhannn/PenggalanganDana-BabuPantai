2ee653396b1807849f07d8c28d41b6589f76d76f
c4lv1 <calvinwinatha88@gmail.com>
2024-12-05 19:33:18 +0700

    add migrations, seeder, and databases file
    after pull this, change database on env to babupantai_db and import the file to your xampp

---
+++ database/seeders/DonationSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Donation;
+use App\Models\Users;
+use Faker\Factory as Faker;
+
+class DonationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+        
+    public function run(): void
+    {
+        //
+        $faker = Faker::create();
+        $users = Users::all(); // Mengambil semua user
+
+        foreach ($users as $user) {
+            // Setiap user melakukan 1-3 donasi
+            foreach (range(1, rand(1, 3)) as $index) {
+                Donation::create([
+                    'user_id' => $user->id,
+                    'amount' => $faker->randomFloat(2, 50000, 500000), // Donasi antara 50rb - 500rb
+                    'payment_method' => $faker->randomElement(['Bank Transfer', 'Credit Card', 'E-Wallet']),
+                    'status' => $faker->randomElement(['completed', 'pending', 'failed']),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+    }
+}
