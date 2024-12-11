3d46f0bb9fae7f5b534c1608612d5b653d7e6b0f
Frendy <frendymarcellino05@gmail.com>
2024-12-11 20:21:20 +0700

    nambah profile displat dan profile update

--- resources/views/layout/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="py-4 bg-dark text-white">
-        <div class="container text-center">
-            <p>&copy; 2024 Babu Pantai Foundation. All rights reserved.</p>
-        </div>
-    </footer>
